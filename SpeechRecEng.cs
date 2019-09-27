a35d6f8cda31f40e39c91a6224ea755de84e73e7
SolaJr <35401733+SolaJr@users.noreply.github.com>
2019-09-26 21:18:22 -0500

    Create SpeechRecEng.cs

---
+++ SpeechRecEng.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class SpeechRecognitionEngine : MonoBehaviour
+{
+    public string[] keywords = new string[] { "Like", "Umm", "Um", "So" };
+    public ConfidenceLevel confidence = ConfidenceLevel.Medium;
+    public float speed = 1;
+
+    public Text results;
+    public Image target;
+
+    protected PhraseRecognizer recognizer;
+    protected string word = "right";
+
+    private void Start()
+    {
+	  LinkedList<String> my_list = new LinkedList<String>(); 
+
+        if (keywords != null)
+        {
+            recognizer = new KeywordRecognizer(keywords, confidence);
+            recognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+            recognizer.Start();
+        }
+    }
+
+    private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        word = args.text;
+        results.text = "You said: <b>" + word + "</b>";
+	
+    }
+
+    private void Update()
+    {
+	var x = x;
+        var y = y;
+
+        switch (word)
+        {
+            case "Like":
+                y = y + 1;
+		
+
+		break;
+            case "Umm":
+                y = y + 2;
+                break;
+	    case "Um":
+                x = x + 2;
+                break;
+            case "So" ;
+                x = x + 1;
+                break;
+        }
+
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (recognizer != null && recognizer.IsRunning)
+        {
+            recognizer.OnPhraseRecognized -= Recognizer_OnPhraseRecognized;
+            recognizer.Stop();
+        }
+    }
+}
